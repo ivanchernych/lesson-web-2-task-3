2cb5d6819ea633b3c62ef1a12b59ecbd11d4e3e4
ivan <d1zzzy.project@gmail.com>
2023-03-25 11:54:03 +0300

    task

---
+++ main.py
@@ -0,0 +1,13 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index/<title>')
+def index(title):
+    return flask.render_template('base.html', title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=8080,  host='127.0.0.1')
